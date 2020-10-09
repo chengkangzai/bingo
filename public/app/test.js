30cc396e224d8cfaffb19db87c57f707686500de
chengkangzai <pycck@hotmail.com>
2020-10-09 15:28:23 +0800

    instead of stating the function name in html, bind in jquery

---
+++ public/app/test.js
@@ -0,0 +1,37 @@
+var string = [
+    `δ(XII,NULL) = {Rej},	δ(XII,f) = {Rej},	δ(XII,o) = {Rej},	δ(XII,r) = {Rej},	δ(XII,i) = {Rej},	δ(XII,n) = {Rej},	δ(XII,a) = {Rej},	δ(XII,g) = {Rej},	δ(XII,e) = {XII},	δ(XII,() = {Rej},	δ(XII,1) = {Rej},	δ(XII,0) = {Rej},	δ(XII,)) = {Rej},	δ(XII,:) = {Rej},	δ(XII,p) = {Rej},	δ(XII,t) = {Rej},
+    `,
+    `δ(XIII,NULL) = {Rej},	δ(XIII,f) = {Rej},	δ(XIII,o) = {Rej},	δ(XIII,r) = {Rej},	δ(XIII,i) = {Rej},	δ(XIII,n) = {Rej},	δ(XIII,a) = {Rej},	δ(XIII,g) = {Rej},	δ(XIII,e) = {Rej},	δ(XIII,() = {XIII},	δ(XIII,1) = {Rej},	δ(XIII,0) = {Rej},	δ(XIII,)) = {Rej},	δ(XIII,:) = {Rej},	δ(XIII,p) = {Rej},	δ(XIII,t) = {Rej},
+    `,
+    `δ(XIV,NULL) = {Rej},	δ(XIV,f) = {Rej},	δ(XIV,o) = {Rej},	δ(XIV,r) = {Rej},	δ(XIV,i) = {Rej},	δ(XIV,n) = {Rej},	δ(XIV,a) = {Rej},	δ(XIV,g) = {Rej},	δ(XIV,e) = {Rej},	δ(XIV,() = {Rej},	δ(XIV,1) = {XIV},	δ(XIV,0) = {Rej},	δ(XIV,)) = {Rej},	δ(XIV,:) = {Rej},	δ(XIV,p) = {Rej},	δ(XIV,t) = {Rej},
+    `,
+    `δ(XV,NULL) = {Rej},	δ(XV,f) = {Rej},	δ(XV,o) = {Rej},	δ(XV,r) = {Rej},	δ(XV,i) = {Rej},	δ(XV,n) = {Rej},	δ(XV,a) = {Rej},	δ(XV,g) = {Rej},	δ(XV,e) = {Rej},	δ(XV,() = {Rej},	δ(XV,1) = {Rej},	δ(XV,0) = {XV},	δ(XV,)) = {Rej},	δ(XV,:) = {Rej},	δ(XV,p) = {Rej},	δ(XV,t) = {Rej},
+    `,
+    `δ(XVI,NULL) = {Rej},	δ(XVI,f) = {Rej},	δ(XVI,o) = {Rej},	δ(XVI,r) = {Rej},	δ(XVI,i) = {Rej},	δ(XVI,n) = {Rej},	δ(XVI,a) = {Rej},	δ(XVI,g) = {Rej},	δ(XVI,e) = {Rej},	δ(XVI,() = {Rej},	δ(XVI,1) = {Rej},	δ(XVI,0) = {Rej},	δ(XVI,)) = {XVI},	δ(XVI,:) = {Rej},	δ(XVI,p) = {Rej},	δ(XVI,t) = {Rej},
+    `,
+    `δ(XVII,NULL) = {Rej},	δ(XVII,f) = {Rej},	δ(XVII,o) = {Rej},	δ(XVII,r) = {Rej},	δ(XVII,i) = {Rej},	δ(XVII,n) = {Rej},	δ(XVII,a) = {Rej},	δ(XVII,g) = {Rej},	δ(XVII,e) = {Rej},	δ(XVII,() = {Rej},	δ(XVII,1) = {Rej},	δ(XVII,0) = {Rej},	δ(XVII,)) = {Rej},	δ(XVII,:) = {XVII},	δ(XVII,p) = {Rej},	δ(XVII,t) = {Rej},
+    `,
+    `δ(XVIII,NULL) = {Rej},	δ(XVIII,f) = {Rej},	δ(XVIII,o) = {Rej},	δ(XVIII,r) = {Rej},	δ(XVIII,i) = {Rej},	δ(XVIII,n) = {Rej},	δ(XVIII,a) = {Rej},	δ(XVIII,g) = {Rej},	δ(XVIII,e) = {Rej},	δ(XVIII,() = {Rej},	δ(XVIII,1) = {Rej},	δ(XVIII,0) = {Rej},	δ(XVIII,)) = {Rej},	δ(XVIII,:) = {Rej},	δ(XVIII,p) = {XVIII},	δ(XVIII,t) = {Rej},
+    `,
+    `δ(XIX,NULL) = {Rej},	δ(XIX,f) = {Rej},	δ(XIX,o) = {Rej},	δ(XIX,r) = {XIX},	δ(XIX,i) = {Rej},	δ(XIX,n) = {Rej},	δ(XIX,a) = {Rej},	δ(XIX,g) = {Rej},	δ(XIX,e) = {Rej},	δ(XIX,() = {Rej},	δ(XIX,1) = {Rej},	δ(XIX,0) = {Rej},	δ(XIX,)) = {Rej},	δ(XIX,:) = {Rej},	δ(XIX,p) = {Rej},	δ(XIX,t) = {Rej},
+    `,
+    `δ(XX,NULL) = {Rej},	δ(XX,f) = {Rej},	δ(XX,o) = {Rej},	δ(XX,r) = {Rej},	δ(XX,i) = {XX},	δ(XX,n) = {Rej},	δ(XX,a) = {Rej},	δ(XX,g) = {Rej},	δ(XX,e) = {Rej},	δ(XX,() = {Rej},	δ(XX,1) = {Rej},	δ(XX,0) = {Rej},	δ(XX,)) = {Rej},	δ(XX,:) = {Rej},	δ(XX,p) = {Rej},	δ(XX,t) = {Rej},
+    `,
+    `δ(XXI,NULL) = {Rej},	δ(XXI,f) = {Rej},	δ(XXI,o) = {Rej},	δ(XXI,r) = {Rej},	δ(XXI,i) = {Rej},	δ(XXI,n) = {XXI},	δ(XXI,a) = {Rej},	δ(XXI,g) = {Rej},	δ(XXI,e) = {Rej},	δ(XXI,() = {Rej},	δ(XXI,1) = {Rej},	δ(XXI,0) = {Rej},	δ(XXI,)) = {Rej},	δ(XXI,:) = {Rej},	δ(XXI,p) = {Rej},	δ(XXI,t) = {Rej},
+    `,
+    `δ(XXII,NULL) = {Rej},	δ(XXII,f) = {Rej},	δ(XXII,o) = {Rej},	δ(XXII,r) = {Rej},	δ(XXII,i) = {Rej},	δ(XXII,n) = {Rej},	δ(XXII,a) = {Rej},	δ(XXII,g) = {Rej},	δ(XXII,e) = {Rej},	δ(XXII,() = {Rej},	δ(XXII,1) = {Rej},	δ(XXII,0) = {Rej},	δ(XXII,)) = {Rej},	δ(XXII,:) = {Rej},	δ(XXII,p) = {Rej},	δ(XXII,t) = {XXII},
+    `,
+    `δ(XXIII,NULL) = {Rej},	δ(XXIII,f) = {Rej},	δ(XXIII,o) = {Rej},	δ(XXIII,r) = {Rej},	δ(XXIII,i) = {Rej},	δ(XXIII,n) = {Rej},	δ(XXIII,a) = {Rej},	δ(XXIII,g) = {Rej},	δ(XXIII,e) = {Rej},	δ(XXIII,() = {XXIII},	δ(XXIII,1) = {Rej},	δ(XXIII,0) = {Rej},	δ(XXIII,)) = {Rej},	δ(XXIII,:) = {Rej},	δ(XXIII,p) = {Rej},	δ(XXIII,t) = {Rej},
+    `,
+    `δ(XXIV,NULL) = {Rej},	δ(XXIV,f) = {Rej},	δ(XXIV,o) = {Rej},	δ(XXIV,r) = {Rej},	δ(XXIV,i) = {XXIV},	δ(XXIV,n) = {Rej},	δ(XXIV,a) = {Rej},	δ(XXIV,g) = {Rej},	δ(XXIV,e) = {Rej},	δ(XXIV,() = {Rej},	δ(XXIV,1) = {Rej},	δ(XXIV,0) = {Rej},	δ(XXIV,)) = {Rej},	δ(XXIV,:) = {Rej},	δ(XXIV,p) = {Rej},	δ(XXIV,t) = {Rej},    `,
+    `δ(XXV,NULL) = {Rej},	δ(XXV,f) = {Rej},	δ(XXV,o) = {Rej},	δ(XXV,r) = {Rej},	δ(XXV,i) = {Rej},	δ(XXV,n) = {Rej},	δ(XXV,a) = {Rej},	δ(XXV,g) = {Rej},	δ(XXV,e) = {Rej},	δ(XXV,() = {Rej},	δ(XXV,1) = {Rej},	δ(XXV,0) = {Rej},	δ(XXV,)) = {XXV},	δ(XXV,:) = {Rej},	δ(XXV,p) = {Rej},	δ(XXV,t) = {Rej},
+    `
+];
+
+for (let i = 0; i < string.length; i++) {
+    console.log(`Its Going with ${i} \n`)
+    string[i].replace("{Rej}", "{Rej}")
+
+    var out1 = string[i].split("\t");
+    console.log(out1.join(""))
+}
